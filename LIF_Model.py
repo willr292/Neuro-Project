32236b2ff1655a7b8d2e9eff6f558bc7b9b7f7d8
Will Rollason <willr292@gmail.com>
2019-03-03 18:12:56 +0000

    stuff

--- LIF_Model.py
+++ LIF_Model.py
@@ -1,23 +1,23 @@
 import numpy as np
-from scipy.integrate import odeint
+import random as rnd
 import matplotlib.pyplot as plt
 
-# PARAMETERS
+# Parameters
 G = 0 # Membrane conductance
 C = 3 # Capacitance
 R = 1 # Membrane Resistance
 E_L = 1 # Resting Potential
 
 Theta_Inf = 0 # Instantaneous threshold
-
+deltaTheta_s =0 #  update to the spike-dependent component of the threshold
 deltaV = 0 # Voltage Addition following spike
 b_s = 0 # Spike induced threshold time constant
-deltaI_j = 0 # Afterspike current amplitudes
+deltaI_j = 0 # Afterspike current amplitudes A_j
 k_j = 0 # Afterspike current time constants
-f_v = 1 # Current fraction following spike
+f_v = 1 # voltage fraction following rest
 a = 0 # Adaptation index of thereshold
 b_v = 0 # Voltage induced threshold time constant
-
+f_j = 0 #
 
 # Time
 T = 0.100 # Simulation length
@@ -30,7 +30,6 @@ V[0] = E_L # set initial to resting potential
 
 # External current
 Ie = np.zeros(len(time))
-# add some external current stuff
 
 # Afterspike current
 Ij = np.empty(len(time))
@@ -44,6 +43,32 @@ Theta_s[0] = 0
 Theta_v = np.empty(len(time))
 Theta_v[0] = 0
 
+# Exicitatory conductance
+g_e = np.empty(len(time))
+g_e[0] = 0
+
+# Inhibitatory conductance
+g_i = np.empty(len(time))
+g_i[0] = 0
+
+def spike_trains(rate, big_t, tau_ref):
+    if 1 <= rate * tau_ref:
+        print("firing rate not possible given refractory period f/p")
+        return []
+
+    exp_rate = rate / (1 - tau_ref * rate)
+
+    spike_train = []
+
+    t = rnd.expovariate(exp_rate)
+
+    while t < big_t:
+        spike_train.append(t)
+        t += tau_ref + rnd.expovariate(exp_rate)
+
+    return spike_train
+
+
 
 def LIFRASCAT():
 
@@ -55,13 +80,24 @@ def LIFRASCAT():
         Theta_v[i] = Theta_v[i - 1] + dTheta_v * dt
 
         dIj = -k_j * Ij[i -1]
-        Ij[i] = I[j - 1] + dIj * dt
+        Ij[i] = Ij[i - 1] + dIj * dt
+
+        dg_e = = -g_e[i -1]/tau_e
+        Ij[i] = Ij[i - 1] + dIj * dt
+
+
         
-        dV = 1/C * (I[i - 1] + G*(V[i - 1] - E_L))
+        dV = 1/C * (Ij[i - 1] + G*(V[i - 1] - E_L) + g_e[i - 1] * (V[i - 1]) + g_i[i - 1] * (V[i - 1] - E_L))
         V[i] = V[i - 1] + dV * dt
 
         # Check for a spike and update voltage etc.
         if V[i] > Theta_v[i] + Theta_s[i]:
-            Ij[i] = 0
+            Ij[i] = f_j * Ij[i] + deltaI_j
             V[i] = E_L + f_v * (V[i] - E_L) - deltaV
+            Theta_s[i] = Theta_s[i] + deltaTheta_s
+        
+        # check if time has passed an event time.
+        if 
+
+    return
 
